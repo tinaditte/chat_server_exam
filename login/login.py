b27b7f212dd8615b456ef2c6f8d695325a764265
TinaDitte <tina.buster@gmail.com>
2019-09-05 14:45:15 +0200

    Initial commit

---
+++ login/login.py
@@ -0,0 +1,13 @@
+import os
+import hashlib
+
+def checking(username, password):
+    if os.path.isfile('../users/' + username):
+        with open('../users/' + username, 'rb') as file_handle:
+            hashedpass = file_handle.read(32)
+        if password.digest() == hashedpass:
+            return True
+        else:
+            return False
+    else:
+        return False
---
+++ login/register.py
@@ -0,0 +1,26 @@
+import os, hashlib, random, string
+
+def generator_strong(size):
+    number = string.digits
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    symbol = string.punctuation
+    return ''.join(random.choice(number + lowercase + uppercase + symbol) for x in range(size))
+
+def create_user(username):
+    if os.path.isfile('../users/' + username):
+        print("Username is taken")
+
+def create_password(username, password):
+    password = hashlib.sha256(password.encode())
+    saltylength = os.path.getsize("../secret/salty")
+
+    with open("../secret/salty", 'r', encoding="utf-8") as file_handle:
+        saltystring = file_handle.read(saltylength)
+
+    password.update(saltystring.encode())
+
+    with open("../users/" + username, 'wb') as file_handle:
+        file_handle.write(password.digest())
+
+
